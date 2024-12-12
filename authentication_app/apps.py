fa5611479befa1c70c0858428f0ce386a3d21222
AryaAppaji <maappaji111@gmail.com>
2024-12-12 18:16:52 +0530

    Added env based setup

--- authentication_app/apps.py
+++ authentication_app/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class AuthenticationAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authentication_app'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "authentication_app"
